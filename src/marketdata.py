f9d8bafd1b7ef7f8a0eee1b155634079604b6a77
root <romeubertho@gmail.com>
2018-06-03 20:58:15 -0300

    add marketdata

---
+++ src/marketdata.py
@@ -0,0 +1,42 @@
+"""A MarketData class responsible to download only B3 futures, options and stocks data.
+
+by Romeu Bertho Junior
+
+Example:
+
+>>> import marketdata
+>>> md=marketdata.MarketData()
+>>> md.get_futures(date='20180102') # download futures marketdata
+"""
+import os
+import ftplib
+from b3constants import *
+
+
+class MarketData:
+    ftp = None
+    # Initialization method (called by class instantiation).
+
+    def __init__(self):
+        self.ftp = ftplib.FTP(b3URL)
+        self.ftp.login()
+
+    def get_futures(self, date, path=os.getcwd()):
+        self.ftp.cwd(b3FUTURES)
+        filename = b3TRADE+date+'.zip'
+        print('Downloading ',filename)
+        path +='/data/futures/'+filename
+        res = self.ftp.retrbinary("RETR " + filename, open(path, 'wb').write)
+        print(res)
+
+    def get_options(self, date=''):
+        pass
+
+    def get_stocks(self, date=''):
+        pass
+    
+    def run(self):
+        pass
+    
+    if __name__ == '__main__':
+        run()
